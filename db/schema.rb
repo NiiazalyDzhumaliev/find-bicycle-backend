31c276ce298dd7adf4a76f1fb1c0a95f1d57d835
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-07 14:54:33 +0600

    DB migration

---
+++ db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_07_084955) do
+
+  create_table "bicycles", force: :cascade do |t|
+    t.string "model"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.integer "bicycle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bicycle_id"], name: "index_favourites_on_bicycle_id"
+  end
+
+  add_foreign_key "favourites", "bicycles"
+end
