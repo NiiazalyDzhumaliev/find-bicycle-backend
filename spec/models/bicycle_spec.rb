8269632d8bc6ba0de219750793684ec3943e8d2a
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-07 16:04:47 +0600

    Add url presence validation spec

--- spec/models/bicycle_spec.rb
+++ spec/models/bicycle_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Bicycle, type: :model do
   it { should have_many(:favourites) }
   it { should validate_presence_of(:model) }
   it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:url) }
 end
