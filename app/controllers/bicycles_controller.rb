19111e0a2dd8035e0db018a8563fd6542fa2301f
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-07 15:20:05 +0600

    Add bicycles controller methods

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,2 +1,17 @@
 class BicyclesController < ApplicationController
+  def index
+    @bicycles = Bicycle.all
+    json.response(@bicycles)
+  end
+
+  def create
+    @bicycle = Bicycle.create(bicycle_params)
+    json.response(@bicycle, :created)
+  end
+
+  private
+
+  def bicycles_params
+    params.permit(:model, :description)
+  end
 end
