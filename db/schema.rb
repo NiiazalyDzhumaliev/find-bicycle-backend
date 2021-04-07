69884899723ed049e69af14f6233018d868aed0e
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-07 18:55:20 +0600

    Migrate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_084955) do
+ActiveRecord::Schema.define(version: 2021_04_07_124213) do
 
   create_table "bicycles", force: :cascade do |t|
     t.string "model"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_04_07_084955) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["bicycle_id"], name: "index_favourites_on_bicycle_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "favourites", "bicycles"
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
-    email 'example@mail.com'
-    password 'helloworld'
+    email { 'example@mail.com' }
+    password { 'helloworld' }
   end
 end
