e30a7a57de8ad1c284a4d16660b21c32f2d507af
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-07 15:01:22 +0600

    Add bicycle and favourite tests

--- spec/models/bicycle_spec.rb
+++ spec/models/bicycle_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Bicycle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:favourites) }
+  it { should validate_presence_of(:model) }
+  it { should validate_presence_of(:description) }
 end
