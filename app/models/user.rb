67710795cfc3789248e14012da0f476f27349399
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-08 15:48:26 +0600

    Add favourite bicycles to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :favourites, dependent: :destroy
+  has_many :favourite_bicycles, through: :favourites, source: :bicycle
   validates_presence_of :name, :email, :password_digest
 end
