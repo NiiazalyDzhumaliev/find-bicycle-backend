d94dc8d51f371e67ff62a968065751fed4b604fc
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-08 16:36:44 +0600

    Create favourite user bicycle

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,4 +3,15 @@ class FavouritesController < ApplicationController
     @favourites = current_user.favourite_bicycles
     json_response(@favourites)
   end
+
+  def create
+    @favourite = current_user.favourites.create!(favourite_params)
+    json_response(@favourite, :created)
+  end
+
+  private
+
+  def favourite_params
+    params.permit(:bicycle_id)
+  end
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,5 +1,5 @@
 class Favourite < ApplicationRecord
   belongs_to :bicycle
   belongs_to :user
-  validates_presence_of :user_id, :bicycle_id
+  validates_presence_of :bicycle_id
 end
