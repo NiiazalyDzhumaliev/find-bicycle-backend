f4ad77a47589d8acbd173361ac76b6748af5a4d6
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-08 14:10:34 +0600

    Add colon to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :bicycles, only: [:index, :create]
-  post 'auth/login', to 'authentication#authenticate'
+  post 'auth/login', to: 'authentication#authenticate'
 end
