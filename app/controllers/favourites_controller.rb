3b20267c5f5c7505faedb16d10d9f5886ab434e0
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-08 16:24:06 +0600

    Showing favourite bikes

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,6 @@
 class FavouritesController < ApplicationController
+  def index
+    @favourites = current_user.favourite_bicycles
+    json_response(@favourites)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :bicycles, only: [:index, :create]
+  
+  resources :favourites
+  
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
