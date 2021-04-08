e573983b2a2847badf92b7bb9ad5ad8962ff5432
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-08 14:36:00 +0600

    To unlike bicycles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :favourites
+  has_many :favourites, dependent: :destroy
   validates_presence_of :name, :email, :password_digest
 end
