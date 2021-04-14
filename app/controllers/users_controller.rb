248a9bf3aef91ea49384803ff8d73abf40ac89c8
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-14 14:50:33 +0600

    Check if user exists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
+
+  def already_registered?
+    User.where(email: user_params[:email]).exists?
+  end
+
   def create
-    user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
-    json_response(response, :created)
+    if !already_registered?
+      user = User.create!(user_params)
+      auth_token = AuthenticateUser.new(user.email, user.password).call
+      response = { message: Message.account_created, auth_token: auth_token }
+      json_response(response, :created)
+    else
+      json_response(message: Message.account_not_created, auth_token: '')
+    end
   end
 
   private
